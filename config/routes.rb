7e0adaec4112fccd1df4743d35236964167e5a8c
Kamal Heydarov <kheydarov84@gmail.com>
2018-08-08 02:59:03 +0000

    create new template, action and route

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
   root 'welcome#home'
   get 'about', to: 'welcome#about'
+  
+  resources :articles
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
