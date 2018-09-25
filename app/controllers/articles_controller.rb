06ff9abacd7202fba81b8df736f1e0dee8e15749
Kamal Heydarov <kheydarov84@gmail.com>
2018-09-25 02:26:09 +0000

    Added secure password

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -20,7 +20,7 @@ class ArticlesController < ApplicationController
   end
   
   def update
-      if @article.update(article_params)
+    if @article.update(article_params)
       flash[:success] = "Article was successfully updated"
       redirect_to article_path(@article)
     else 
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,5 @@ class User < ActiveRecord::Base
             length: { minimum:5, maximuim: 105 },
             uniqueness: { case_sensitive: false },
             format: { with: VALID_EMAIL_REGEX }
+  has_secure_password
 end
